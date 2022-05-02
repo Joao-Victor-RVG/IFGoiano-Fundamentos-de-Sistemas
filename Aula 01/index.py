24d066a9fe081fbf9a4220942c7466177a370213
João Victor Rocha Vilela Godoi <80182535+Joao-Victor-RVG@users.noreply.github.com>
2022-05-02 15:09:30 -0300

    My first commit

---
+++ Aula 01/index.py
@@ -0,0 +1,14 @@
+from tkinter import N
+
+
+nome_aluno = ['Alexandre' 'Vitor' 'Jean' 'Joao']
+with open('alunos.txt' , 'w') as writer:
+    for aluno in nome_aluno:
+        writer.write('aluno +\n')
+    
+
+
+
+
+
+

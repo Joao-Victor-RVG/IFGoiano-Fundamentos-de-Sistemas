c7667831e3621be0b353ed7d3a940d14c93b7567
João Victor Rocha Vilela Godoi <80182535+Joao-Victor-RVG@users.noreply.github.com>
2022-06-26 22:50:47 -0300

    Updates

---
+++ Aula 03/ex001.py
@@ -0,0 +1,14 @@
+from gtts import gTTS
+from playsound import playsound
+
+audio = 'meusom.mp3'
+idioma = 'fr'
+conversao = gTTS(
+    text = 'teste',
+    lang = idioma
+)
+
+conversao.save(audio)
+
+
+
